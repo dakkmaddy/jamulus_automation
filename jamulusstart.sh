614b22d2570b4ab979581b013c783bcbc8175ed8
dakkmaddy <42662940+dakkmaddy@users.noreply.github.com>
2025-03-10 19:16:15 -0700

    Update jamulusstart.sh

--- jamulusstart.sh
+++ jamulusstart.sh
@@ -4,7 +4,7 @@ pubip=$(curl http://icanhazip.com)
 if [ $runjam = "0" ]
 then
 	echo "[-] runjam is $runjam, launching jamulus"
-	/usr/bin/jamulus-headless --nogui --server -z -u 6 --serverpublicip $pubip --directoryaddress rock.jamulus.io:22424 -l /var/log/jamulus.log --jsonrpcport 10113 --jsonrpcsecretfile /srv/Jamulus/rpcsecret.txt -w /srv/Jamulus/jamuluswelcome.txt --serverinfo "willowjamz2025;richmond;us"
+	/usr/bin/jamulus-headless --nogui --server -z -u 6 --serverpublicip $pubip --directoryaddress rock.jamulus.io:22424 -l /var/log/jamulus.log --jsonrpcport 10113 --jsonrpcsecretfile /srv/Jamulus/rpc.txt -w /srv/Jamulus/jamuluswelcome.txt --serverinfo "willowjamz2025;richmond;us"
 else 
 	echo "[-] runjam is $runjam, which means jamulus is running"
 fi
